f88dd5765c840f4444b7c20aaa18b7a5fabf3e85
Dmitrii Bordukov <dabordukov@mail.ru>
2026-09-20 20:20:04 +0300

    task1_2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    let mut sum: u128 = 0;
+    loop {
+        input.clear();
+        if io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line")
+            == 0
+        {
+            break;
+        }
+
+        let input = input.trim();
+
+        if input == "-1" {
+            break;
+        }
+
+        if input.is_empty() {
+            continue;
+        }
+
+        match input.parse::<u128>() {
+            Ok(number) if number > 0 => sum += number,
+            _ => {
+                println!("NaN");
+                return;
+            }
+        }
+    }
+
+    println!("{sum}")
 }
